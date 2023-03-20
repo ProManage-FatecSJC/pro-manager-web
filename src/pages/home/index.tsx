2d16694b8f14f8e76d0e95411db2fd933b1df361
vbuarque <viniciusbuarque17@gmail.com>
2023-03-20 19:50:26 -0300

    feat: Finalizando o front end do componente de filtros de pesquisa

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -16,7 +16,34 @@ export function Home() {
                     </div>
                     <button>Adicionar parceiro</button>
                 </header>
-
+                <div className="filter_container">
+                    <div className="filter">
+                        <label htmlFor="status">Status</label>
+                        <select name="status" id="status">
+                            <option value="all">Todos</option>
+                            <option value="active">Em prospecção</option>
+                            <option value="active">Primeiro contato feito</option>
+                            <option value="active">Primeira reunião marcada/realizada</option>
+                            <option value="active">Documentação enviada/em análise (Parceiro)</option>
+                            <option value="active">Documentação devolvida (Em análise Academy)</option>
+                            <option value="active">Documentação devolvida (Em análise Legal)</option>
+                            <option value="active">Documentação analisada devolvida (Parceiro)</option>
+                            <option value="active">Em preparação de Executive Sumary (Academy)</option>
+                            <option value="active">ES em análise (Legal)</option>
+                            <option value="active">ES em análise (Academy Global)</option>
+                            <option value="active">Pronto para assinatura</option>
+                            <option value="active">Parceria Firmada</option>
+                        </select>
+                    </div>
+                    <div className="filter">
+                        <label htmlFor="status">Ordem alfabética</label>
+                        <select name="status" id="status">
+                            <option value="all">Todos</option>
+                            <option value="active">De A a Z</option>
+                            <option value="active">De Z a A</option>
+                        </select>
+                    </div>
+                </div>
                 <table>
                     <tbody>
                         <tr>
