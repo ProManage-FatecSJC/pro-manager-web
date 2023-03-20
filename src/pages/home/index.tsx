775e0083bebd65112818c802951df1fffda9da32
vbuarque <viniciusbuarque17@gmail.com>
2023-03-20 10:35:53 -0300

    feat: Finalizando a tela home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,32 @@
-export function Home() {
-    return <h1>Home</h1>
-}
+import { FileSearch } from 'phosphor-react';
+import './style.scss';
 
+
+export function Home() {
+    return (
+        <div className="register-container">
+            <main>
+                <header className='table_header'>
+                    <h1>Parceiros</h1>
+                    <button>Adicionar parceiro</button>
+                </header>
+                <table>
+                    <thead>
+                        <tr>
+                            <th className='th_name'>Nome</th>
+                            <th className='th_email'>E-mail</th>
+                            <th className='th_edit'>Editar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </main>
+        </div>
+    )
+}
