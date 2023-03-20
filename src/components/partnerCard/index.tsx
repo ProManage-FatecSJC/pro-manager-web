9b9bac18df38244947909d95c6db29029dba5f32
vbuarque <viniciusbuarque17@gmail.com>
2023-03-20 10:46:47 -0300

    feat: Criando o componente de partnerCard e setando suas propriedades

---
+++ src/components/partnerCard/index.tsx
@@ -0,0 +1,12 @@
+type PartnerCardTypes = {
+    image: string;
+    name: string;
+    responsibilityPartnetName: string;
+    status: string;
+}
+
+export function PartnerCard({image, name, responsibilityPartnetName, status}: PartnerCardTypes) {
+    return (
+        <div></div>
+    )
+}
