66be267c699a08608675795ceb2c140072cff06d
Daniel <daniel.2308@outlook.com>
2023-05-09 11:41:21 -0300

    feat: Iniciando modal de visualização de membro

---
+++ src/components/ModalViewMember/index.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+export function ModalViewMember({ isOpen, setModalOpen }: any) {
+
+    useEffect(() => {
+        function onEsc(event: KeyboardEvent) {
+            if (event.key === 'Escape') {
+                setModalOpen();
+            }
+        }
+        window.addEventListener('keydown', onEsc);
+
+        return () => {
+            window.removeEventListener('keydown', onEsc);
+        }
+    }, [setModalOpen]);
+}
--- src/components/memberCard/index.tsx
+++ src/components/memberCard/index.tsx
@@ -7,18 +7,23 @@ type MemberCardTypes = {
     CNPJ: string
 }
 
-export function MemberCard({ image, name, trade_name, CNPJ }: MemberCardTypes){
+export function MemberCard({ image, name, trade_name, CNPJ }: MemberCardTypes) {
     return (
-        <><td className="container_card">
-            <div className='partner_img'>
-                <img src={image} alt="Foto do parceiro" />
-            </div>
+        <>
+            <button>
 
-            <div className="info_card">
-                <h2>{name}</h2>
-                <p>Nome fantasia: {trade_name}</p>
-                <p>CNPJ: {CNPJ}</p>
-            </div>
-        </td></>
+                <td className="container_card">
+                    <div className='partner_img'>
+                        <img src={image} alt="Foto do parceiro" />
+                    </div>
+
+                    <div className="info_card">
+                        <h2>{name}</h2>
+                        <p>Nome fantasia: {trade_name}</p>
+                        <p>CNPJ: {CNPJ}</p>
+                    </div>
+                </td>
+            </button>
+        </>
     )
 }
